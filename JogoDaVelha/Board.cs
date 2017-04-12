ec82f5cba169611ba6e61449166693702de0507f
Guilherme Galarça Dutra <guilhermegalarcadutra@outlook.com>
2017-04-12 18:11:46 -0300

    12/04/17

---
+++ JogoDaVelha/Board.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    class Board
+    {
+        private int[,] cell;
+        //0 é vazio, 1 é computador e 2 é humano
+
+        public Board()
+        {
+            cell = new int[3, 3];
+        }
+
+        public void Limpar()
+        {
+            for (int y = 0; y < GetComprimentoArray(); y++)
+            {
+                for (int x = 0; x < GetAlturaArray(); x++)
+                {
+                    cell[x, y] = 0;
+                }
+            }
+        }
+
+        public int[,] GetCell()
+        {
+
+            return cell;
+        }
+
+        public void SetCell(int[,] cellA)
+        {
+            cell = cellA;
+        }
+
+
+        public void AcrescentaValor(int x, int y)
+        {
+            AtribuiValorCelula(x, y, RetornaValorCelula(x, y) + 1);
+            //cell[x, y] += 1;
+        }
+
+        public void ZeraValorCelula(int x, int y)
+        {
+            cell[x, y] = 0;
+        }
+
+        public void AtribuiValorCelula(int x, int y, int val)
+        {
+            cell[x, y] = val;
+        }
+
+        public int RetornaValorCelula(int x, int y)
+        {
+            return cell[x, y];
+        }
+
+        public int GetAlturaArray()
+        {
+            return cell.GetLength(0);
+        }
+
+        public int GetComprimentoArray()
+        {
+            return cell.GetLength(1);
+        }
+
+        public int GetVencedor()
+        {
+            int vencedor = -1;
+
+            for (int player = 1; player <= 2; player++)
+            {
+                for (int y = 0; y < cell.GetLength(1); y++) // linhas
+                {
+                    if (cell[0, y] == player && cell[1, y] == player && cell[2, y] == player)
+                    {
+                        vencedor = player;
+                    }
+                }
+
+                for (int x = 0; x < cell.GetLength(0); x++) // colunas
+                {
+                    if (cell[x, 0] == player && cell[x, 1] == player && cell[x, 2] == player)
+                    {
+                        vencedor = player;
+                    }
+                }
+
+                {//Diagonal
+                    if (cell[0, 0] == player && cell[1, 1] == player && cell[2, 2] == player)
+                    {
+                        vencedor = player;
+                    }
+
+                    if (cell[0, 2] == player && cell[1, 1] == player && cell[2, 0] == player)
+                    {
+                        vencedor = player;
+                    }
+                }
+            }
+            if (vencedor == -1)
+            {
+                vencedor = VerificaEmpate();
+            }
+            return vencedor;
+        }
+
+        private int VerificaEmpate()
+        {
+            for (int y = 0; y < GetComprimentoArray(); y++)
+            {
+                for (int x = 0; x < GetAlturaArray(); x++)
+                {
+                    if (RetornaValorCelula(x,y) == 0)
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        static public int Minimax(Board board, int profundidade, int jogador)
+        {
+            if (board.EhFimDeJogo() || profundidade == 0)
+            {
+                return board.CalcularScore();
+            }
+
+            int valor;
+
+            if (jogador == 2) // humano
+            {
+                valor = 9999999;
+                List<Board> possibilidades = board.GetPossibilidades(2);
+                foreach (Board p in possibilidades)
+                {
+                    valor = Math.Min(valor, Minimax(p, profundidade - 1, 1 /* Computador */));
+                }
+            }
+            else // computador
+            {
+                valor = -9999999;
+                List<Board> possibilidades = board.GetPossibilidades(1);
+                foreach (Board p in possibilidades)
+                {
+                    valor = Math.Max(valor, Minimax(p, profundidade - 1, 2 /* Humano */));
+                }
+            }
+
+            return valor;
+        }
+
+        public bool EhFimDeJogo()
+        {
+            if (GetVencedor() > 0)
+            {
+                return true;
+            }
+            for (int y = 0; y < GetComprimentoArray(); y++)
+            {
+                for (int x = 0; x < GetAlturaArray(); x++)
+                {
+                    if (RetornaValorCelula(x, y) == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public int CalcularScore()
+        {
+            if (GetVencedor() == 1)
+            {
+                return 1;
+            }
+            else if (GetVencedor() == 2)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Board GetCopia()
+        {
+            Board minhaCopia = new Board();
+
+            for (int y = 0; y < GetComprimentoArray(); y++)
+            {
+                for (int x = 0; x < GetAlturaArray(); x++)
+                {
+                    //minhaCopia.cell[x, y] = cell[x, y];
+                    minhaCopia.AtribuiValorCelula(x, y, RetornaValorCelula(x,y));
+                }
+            }
+
+            return minhaCopia;
+        }
+
+        public List<Board> GetPossibilidades(int jogador)
+        {
+            List<Board> resultado = new List<Board>();
+
+            for (int y = 0; y < GetComprimentoArray(); y++)
+            {
+                for (int x = 0; x < GetAlturaArray(); x++)
+                {
+                    Board minhaCopia = GetCopia();
+                    //minhaCopia.cell[x, y] = jogador;
+                    minhaCopia.AtribuiValorCelula(x, y, jogador);
+                    resultado.Add(minhaCopia);
+                }
+            }
+
+            return resultado;
+        }
+    }
+}
--- JogoDaVelha/Game1.cs
+++ JogoDaVelha/Game1.cs
@@ -4,79 +4,154 @@ using Microsoft.Xna.Framework.Input;
 
 namespace JogoDaVelha
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+
+        private SpriteBatch spriteBatch;
+
+        private Texture2D cellEmpty;
+        private Texture2D cellO;
+        private Texture2D cellX;
+
+        private Vector2 cellPos;
+
+        private MouseState prevMouseState;
+
+        private int Width;
+        private int Height;
+        private int tamanhoImagem;
+
+        private float countDown = 3;
+
+        private Board board;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+
+            Width = 640;
+            Height = 400;
+            tamanhoImagem = 64;
+
+            board = new Board();
+
+            graphics.PreferredBackBufferWidth = Width;
+            graphics.PreferredBackBufferHeight = Height;
+
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            cellEmpty = Content.Load<Texture2D>("CellEmpty");
+            cellO = Content.Load<Texture2D>("CellO");
+            cellX = Content.Load<Texture2D>("CellX");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            {//simple timer sample
+                countDown -= dt;
+                if (countDown <= 0)
+                {
+                    countDown = 3;
+                    System.Diagnostics.Debug.WriteLine("BOOM!");
+                }
+            }
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
 
-            // TODO: Add your update logic here
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
+            {
+                Vector2 pTeste = new Vector2(Mouse.GetState().Position.X, Mouse.GetState().Position.Y);
+                for (int y = 0; y < board.GetAlturaArray(); y++)
+                {
+                    for (int x = 0; x < board.GetAlturaArray(); x++)
+                    {
+                        Vector2 pMin = new Vector2(x * tamanhoImagem + (Width - tamanhoImagem * board.GetAlturaArray()) / 2, y * tamanhoImagem + (Height - tamanhoImagem * board.GetComprimentoArray()) / 2);
+                        Vector2 pMax = pMin + new Vector2(tamanhoImagem, tamanhoImagem);
 
+                        /*Vector2 pMax = pMin;
+                        pMax.X += 64;
+                        pMax.Y += 64;*/
+
+                        if (((pTeste.X > pMin.X) && (pTeste.X < pMax.X)) && ((pTeste.Y > pMin.Y) && (pTeste.Y < pMax.Y)))
+                        {
+
+                            board.AcrescentaValor(x, y); //board.cell[x,y] +=  1;
+
+
+                            if (board.RetornaValorCelula(x, y) > 2) //if(board.cell[x,y] > 2)
+                            {
+                                board.ZeraValorCelula(x, y); //board.cell[x, y] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int ganhador = board.GetVencedor();
+
+            if (ganhador > 0)
+            {
+                System.Diagnostics.Debug.WriteLine("Ganhador é: " + ganhador);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("jogo empatado!");
+            }
+
+            prevMouseState = Mouse.GetState();
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
+            for (int y = 0; y < board.GetComprimentoArray(); y++)
+            {
+                for (int x = 0; x < board.GetAlturaArray(); x++)
+                {
+                    cellPos = new Vector2(x * tamanhoImagem + (Width - tamanhoImagem * board.GetAlturaArray()) / 2, y * tamanhoImagem + (Height - tamanhoImagem * board.GetComprimentoArray()) / 2);
 
+                    spriteBatch.Draw(cellEmpty, cellPos);
+                    
+
+                    //if(board.cell[x,y] == 1){
+                    if (board.RetornaValorCelula(x, y) == 1)
+                    {
+                        spriteBatch.Draw(cellX, cellPos);
+                    }
+                    //if (board.cell[x, y] == 2){
+                    if (board.RetornaValorCelula(x, y) == 2)
+                    {
+                        spriteBatch.Draw(cellO, cellPos);
+                    }
+                }
+            }
+
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
