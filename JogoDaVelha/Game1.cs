0983b05405a73e195ba957ba54ee98ed022d93e3
Guilherme Galarça Dutra <guilhermegalarcadutra@outlook.com>
2017-04-12 22:24:05 -0300

    Depois da Aula 12/04

--- JogoDaVelha/Game1.cs
+++ JogoDaVelha/Game1.cs
@@ -14,6 +14,8 @@ namespace JogoDaVelha
         private Texture2D cellO;
         private Texture2D cellX;
 
+        private SpriteFont fonteNormal;
+
         private Vector2 cellPos;
 
         private MouseState prevMouseState;
@@ -25,6 +27,8 @@ namespace JogoDaVelha
         private float countDown = 3;
 
         private Board board;
+
+        private UIButton btSair;
 
         public Game1()
         {
@@ -52,9 +56,12 @@ namespace JogoDaVelha
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            cellEmpty = Content.Load<Texture2D>("CellEmpty");
-            cellO = Content.Load<Texture2D>("CellO");
-            cellX = Content.Load<Texture2D>("CellX");
+            cellEmpty   = Content.Load<Texture2D>("Sprites/CellEmpty");
+            cellO       = Content.Load<Texture2D>("Sprites/CellO");
+            cellX       = Content.Load<Texture2D>("Sprites/CellX");
+            fonteNormal = Content.Load<SpriteFont>("Fonts/Normal");
+
+            btSair = new UIButton(new Vector2(10, 10), new Vector2(64, 32), cellEmpty, "Sair", fonteNormal);
         }
 
         protected override void UnloadContent()
@@ -83,6 +90,12 @@ namespace JogoDaVelha
             if (Mouse.GetState().LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
             {
                 Vector2 pTeste = new Vector2(Mouse.GetState().Position.X, Mouse.GetState().Position.Y);
+
+                if (btSair.TesteClick(pTeste))
+                {
+                    Exit();
+                }
+
                 for (int y = 0; y < board.GetAlturaArray(); y++)
                 {
                     for (int x = 0; x < board.GetAlturaArray(); x++)
@@ -127,7 +140,26 @@ namespace JogoDaVelha
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.NonPremultiplied);
+
+            //string titulo = "Tic-Tac-Toe";
+            string titulo = "Tempo: " + (int) gameTime.TotalGameTime.TotalSeconds + " segundos";
+            Vector2 textSize = fonteNormal.MeasureString(titulo);
+            //spriteBatch.DrawString(fonteNormal, "Hello World", textSize, Color.White);
+
+            spriteBatch.DrawString(
+                fonteNormal,
+                titulo, 
+                new Vector2(Width*0.5f,40),    //position 
+                Color.White,            //color
+                0.0f,                   //rotation
+                textSize * 0.5f,        //origin (pivot)
+                Vector2.One,            //scale
+                SpriteEffects.None, 
+                0.0f
+                );
+
+            btSair.Draw(spriteBatch);
 
             for (int y = 0; y < board.GetComprimentoArray(); y++)
             {
@@ -135,18 +167,18 @@ namespace JogoDaVelha
                 {
                     cellPos = new Vector2(x * tamanhoImagem + (Width - tamanhoImagem * board.GetAlturaArray()) / 2, y * tamanhoImagem + (Height - tamanhoImagem * board.GetComprimentoArray()) / 2);
 
-                    spriteBatch.Draw(cellEmpty, cellPos);
+                    spriteBatch.Draw(cellEmpty, cellPos, Color.White);
                     
 
                     //if(board.cell[x,y] == 1){
                     if (board.RetornaValorCelula(x, y) == 1)
                     {
-                        spriteBatch.Draw(cellX, cellPos);
+                        spriteBatch.Draw(cellX, cellPos, Color.Red);
                     }
                     //if (board.cell[x, y] == 2){
                     if (board.RetornaValorCelula(x, y) == 2)
                     {
-                        spriteBatch.Draw(cellO, cellPos);
+                        spriteBatch.Draw(cellO, cellPos, Color.Yellow);
                     }
                 }
             }
---
+++ JogoDaVelha/UIButton.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    class UIButton
+    {
+
+        private Vector2 position, size;
+        private Texture2D background;
+        private String text;
+        private SpriteFont font;
+
+        public UIButton(Vector2 position, Vector2 size, Texture2D background, string text, SpriteFont font)
+        {
+            this.position = position;
+            this.size = size;
+            this.background = background;
+            this.text = text;
+            this.font = font;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(background, position, null, Color.White, 0.0f, Vector2.Zero, new Vector2(size.X / background.Width, size.Y / background.Height), SpriteEffects.None, 0.0f);
+            Vector2 textSize = font.MeasureString(text);
+
+            spriteBatch.DrawString(
+                font,
+                text,
+                position + (size / 2.0f),   //position 
+                Color.White,                //color
+                0.0f,                       //rotation
+                textSize / 2.0f,            //origin (pivot)
+                Vector2.One,                //scale
+                SpriteEffects.None,
+                0.0f
+                );
+        }
+
+        public bool TesteClick(Vector2 pTeste)
+        {
+            if (((pTeste.X > position.X) && (pTeste.X < position.X + size.X)) && ((pTeste.Y > position.Y) && (pTeste.Y < position.Y + size.Y)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
